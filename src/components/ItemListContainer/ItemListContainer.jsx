98ae3d378e3f88be578a8ee8049d685f59a99362
Santiago Muscolo <smuscolo2003@gmail.com>
2023-03-19 01:06:54 -0300

    cooregui un error en el style

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,7 +2,7 @@
 export function ItemListContainer({greeting}) {
     return (
         <>
-        <h1 style={{display: 'flex', justifyContent: 'center', marginTop: '5%', fontFamily: 'Bebas Neue', fontSize: '2rem',}}>{greeting}</h1>
+        <h1 style={{display: 'flex', justifyContent: 'center', marginTop: '5%', fontFamily: 'Bebas Neue', fontSize: '2rem'}}>{greeting}</h1>
         </>
     )
 }
