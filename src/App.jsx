3329b70733725fb90453acc8ac951e607e4a56ec
Santiago Muscolo <smuscolo2003@gmail.com>
2023-04-05 19:32:32 -0300

    agregue algunos estilos

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,16 @@ function App() {
       <div>
         <BrowserRouter>
             <NavBar />
+            <div className='Main'>
             <Routes>
               <Route path='/' element= {<ItemListContainer greeting='Bienvenidos a Moukkai' />}/>
               <Route path='/categoria/:categoria' element= {<ItemListContainer />}/>
-              <Route path='/detail/:id' element= {<ItemDetailContainer />} />
+              <Route path='/detail/:id' element= {<ItemDetailContainer />}/>
               <Route path='/cart' element= {<CartContainer />} />
               {/* <Route path='/404notfound' element={<404NotFound/>}/> */}
               {/* <Route path='*' element={<Navigate to={'/404notfound'}/>}/> */}
               </Routes>
+            </div>
         </BrowserRouter>
       </div>
 
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -6,11 +6,14 @@ export const ItemDetail = ({product}) => {
         console.log(product)
         return(
             <>
-                <div>
-                    <p>{product.id}</p>
+                <div className="detail">
                     <img src={product.img} alt="" />
+                    <div className="contenido-detail">
+                        <h5>{product.name}</h5>
+                        <p>{product.description}</p>
+                        <Contador />
+                    </div>
                 </div>
-                <Contador />
             </>
         )
 }
